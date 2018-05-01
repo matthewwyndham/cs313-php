969b4dbddbb8167164b80e31c82fe17ce7694246
Matt Wyndham <matthewwyndham@gmail.com>
2018-05-01 16:47:46 -0600

    mattwyndham autocommit

--- web/week02.php
+++ web/week02.php
@@ -11,7 +11,7 @@
         for ($i = 1; $i <= 10; $i++) {
             echo "<div id=\"div$i\"";
             if ($i / 2 == 0) {
-                echo " style=\"color: red;\" ";
+                echo " style=\"background-color: red;\" ";
             }
             echo "></div>";
         }
