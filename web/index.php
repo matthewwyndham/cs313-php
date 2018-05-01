c33cab3bfe33c61b27be7794a40e4228807d792e
Matt Wyndham <matthewwyndham@gmail.com>
2018-05-01 16:45:41 -0600

    mattwyndham autocommit

--- web/index.php
+++ web/index.php
@@ -4,13 +4,7 @@
 
     <h1>The Index</h1>
     <a href="hello.php">hello.php</a>
-    <?php
-    echo "Hello World!";
-    ?>
-    
-    <?php
-    echo (4 + "cat");
-    ?>
+    <a href="week02.php">week02.php</a>
 
 </body>
 </html>
---
+++ web/week02.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Generate divs!</title>
+    </head>
+    <body>
+
+        <h1>Div generator</h1>
+
+        <?php
+        for ($i = 1; $i <= 10; $i++) {
+            echo "<div id=\"div$i\"";
+            if ($i / 2 == 0) {
+                echo " style=\"color: red;\" ";
+            }
+            echo "></div>";
+        }
+        
+        ?>
+
+    </body>
+</html>
