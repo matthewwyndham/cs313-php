1247616669ebd6d0911f36f8cdfd55ea686176e2
Matt Wyndham <matthewwyndham@gmail.com>
2018-05-03 16:01:31 -0600

    mattwyndham autocommit

--- web/week02/home.php
+++ web/week02/home.php
@@ -1,15 +1,21 @@
 <!doctype html>
-
-<html lang="en">
+<html>
 <head>
-  <meta charset="utf-8">
-  <title>Homepage</title>
-  <link rel="stylesheet" href="style.css">
+  <title>Hello</title>
+    
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/homepage.css">
 </head>
 
 <body>
+    <header><?php include 'navbar.php'?></header>
     
-
-  <script src="script.js"></script>
+	<main>
+        <p>You are not logged in</p>
+    </main>
+    
+    <script type="text/javascript"  src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js">
+    </script>
 </body>
 </html>
