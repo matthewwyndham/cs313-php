9f1b48a607cab8f7ce8c28d646609cfcba23d381
Matt Wyndham <matthewwyndham@gmail.com>
2018-05-03 16:47:16 -0600

    mattwyndham autocommit

--- web/week02/home.php
+++ web/week02/home.php
@@ -19,6 +19,9 @@
             <?php if(isset($_SESSION['user'])) {echo $_SESSION['user'];} else {echo 'You are not logged in';} ?>
             .
         </p>
+        <p>
+        <?php var_dump($_SESSION['user']); ?>
+        </p>
     </main>
     
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
