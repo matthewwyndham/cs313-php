f196e79d311ff7c23e1db399acdb07db1be7087e
Matt Wyndham <matthewwyndham@gmail.com>
2018-05-03 16:39:22 -0600

    mattwyndham autocommit

--- web/week02/login.php
+++ web/week02/login.php
@@ -12,8 +12,8 @@
     <header><?php include 'navbar.php'?></header>
     
 	<main>
-        <p><a src="#">Log in as Administrator</a></p>
-        <p><a src="#">Log in as Tester</a></p>
+        <p><a href="home.php" onclick="<?php $_SESSION['user'] = 'Administrator' ?>">Log in as Administrator</a></p>
+        <p><a href="home.php" onclick="<?php $_SESSION['user'] = 'Tester' ?>">Log in as Tester</a></p>
     </main>
     
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
