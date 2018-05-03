60f3342dc71ca231cf9d8a7269fb8181e11d38bb
Matt Wyndham <matthewwyndham@gmail.com>
2018-05-03 16:03:21 -0600

    mattwyndham autocommit

--- web/week02/navbar.php
+++ web/week02/navbar.php
@@ -1,7 +1,7 @@
         <!-- Bootstrap Navbar -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-        <a class="navbar-brand" href="#">CS313 Portfolio</a>
+        <a class="navbar-brand" href="#">Week 02 : Work Together</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
