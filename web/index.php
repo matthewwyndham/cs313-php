4bdfa34a7d3b1ab3192a2d55e5043bc05d547421
Matt Wyndham <matthewwyndham@gmail.com>
2018-05-03 23:48:14 -0600

    mattwyndham autocommit

--- web/index.php
+++ web/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" type="text/css" href="skeleton.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     
 </head>
 <body>
@@ -16,12 +16,12 @@
         <div class="row">
             <h2 class="five">Week 02</h2>
             <p class="seven"><a href="week02/week02.php">week02.php prep</a></p>
-             </div><div class="row"><p class="seven"><a href="week02/home.php">week02 team assignment</a></p>
+            <p class="seven"><a href="week02/home.php">week02 team assignment</a></p>
         </div><div class="row">
             <h2 class="five">Week 03</h2>
             <p class="seven"><a href="comingsoon.html">Team Activity</a></p>
-             </div><div class="row"><p class="seven"><a href="comingsoon.html">Assignment</a></p>
-             </div><div class="row"><p class="seven"><a href="comingsoon.html">Project</a></p>
+            <p class="seven"><a href="comingsoon.html">Assignment</a></p>
+            <p class="seven"><a href="comingsoon.html">Project</a></p>
         </div><div class="row">
             <h2 class="five">Week 04</h2>
             <p class="seven"><a href="comingsoon.html">Team Activity</a></p>
@@ -85,5 +85,9 @@
             <p class="seven"><a href="comingsoon.html">Project</a></p>
         </div>
     </div>
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 </body>
 </html>
