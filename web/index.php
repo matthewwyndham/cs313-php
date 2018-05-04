26353cca761cd74b8b9ccd5a0d879edeaf92e8b4
Matt Wyndham <matthewwyndham@gmail.com>
2018-05-03 20:30:26 -0600

    mattwyndham autocommit

--- web/index.php
+++ web/index.php
@@ -11,70 +11,72 @@
     <p>I'm from Washington and I served my mission in Japan. I love science fiction, video games, algorithms, space, and soda. My favorite animal is the noble, majestic fox. I aspire to be like unto the fox, shrewd and quick. A predator in it's sphere. I also enjoy poetry and cinema, I feel they are the same (when it's good cinema). I also am a fan of programming, but we all are, aren't we?</p>
     
     <h1>The Index</h1>
+    <div class="container">
     <h2>Week 02</h2>
     <p><a href="week02/week02.php">week02.php prep</a></p>
     <p><a href="week02/home.php">week02 team assignment</a></p>
-    <hr/>
+    
     <h2>Week 03</h2>
     <p><a href="comingsoon.html">Team Activity</a></p>
     <p><a href="comingsoon.html">Assignment</a></p>
     <p><a href="comingsoon.html">Project</a></p>
-    <hr/>
+    
     <h2>Week 04</h2>
     <p><a href="comingsoon.html">Team Activity</a></p>
     <p><a href="comingsoon.html">Assignment</a></p>
     <p><a href="comingsoon.html">Project</a></p>
-    <hr/>
+    
     <h2>Week 05</h2>
     <p><a href="comingsoon.html">Team Activity</a></p>
     <p><a href="comingsoon.html">Assignment</a></p>
     <p><a href="comingsoon.html">Project</a></p>
-    <hr/>
+    
     <h2>Week 06</h2>
     <p><a href="comingsoon.html">Team Activity</a></p>
     <p><a href="comingsoon.html">Assignment</a></p>
     <p><a href="comingsoon.html">Project</a></p>
-    <hr/>
+    
     <h2>Week 07</h2>
     <p><a href="comingsoon.html">Team Activity</a></p>
     <p><a href="comingsoon.html">Assignment</a></p>
     <p><a href="comingsoon.html">Project</a></p>
-    <hr/>
+    
     <h2>Week 08</h2>
     <p><a href="comingsoon.html">Team Activity</a></p>
     <p><a href="comingsoon.html">Assignment</a></p>
     <p><a href="comingsoon.html">Project</a></p>
-    <hr/>
+    
     <h2>Week 09</h2>
     <p><a href="comingsoon.html">Team Activity</a></p>
     <p><a href="comingsoon.html">Assignment</a></p>
     <p><a href="comingsoon.html">Project</a></p>
-    <hr/>
+    
     <h2>Week 10</h2>
     <p><a href="comingsoon.html">Team Activity</a></p>
     <p><a href="comingsoon.html">Assignment</a></p>
     <p><a href="comingsoon.html">Project</a></p>
-    <hr/>
+    
     <h2>Week 11</h2>
     <p><a href="comingsoon.html">Team Activity</a></p>
     <p><a href="comingsoon.html">Assignment</a></p>
     <p><a href="comingsoon.html">Project</a></p>
-    <hr/>
+    
     <h2>Week 12</h2>
     <p><a href="comingsoon.html">Team Activity</a></p>
     <p><a href="comingsoon.html">Assignment</a></p>
     <p><a href="comingsoon.html">Project</a></p>
-    <hr/>
+    
     <h2>Week 13</h2>
     <p><a href="comingsoon.html">Team Activity</a></p>
     <p><a href="comingsoon.html">Assignment</a></p>
     <p><a href="comingsoon.html">Project</a></p>
-    <hr/>
+    
     <h2>Week 14</h2>
     <p><a href="comingsoon.html">Team Activity</a></p>
     <p><a href="comingsoon.html">Assignment</a></p>
     <p><a href="comingsoon.html">Project</a></p>
-    <hr/>
+    
+    </div>
 
 </body>
 </html>
