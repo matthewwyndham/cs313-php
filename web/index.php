79afa7e8321a73aa39655069e8edd79272bd4f7d
Matt Wyndham <matthewwyndham@gmail.com>
2018-05-04 00:23:27 -0600

    mattwyndham autocommit

--- web/index.php
+++ web/index.php
@@ -17,79 +17,102 @@
     <div class="alert alert-dark" role="alert">I'm from Washington and I served my mission in Japan. I love science fiction, video games, algorithms, space, and soda. My favorite animal is the noble, majestic fox. I aspire to be like unto the fox, shrewd and quick. A predator in it's sphere. I also enjoy poetry and cinema, I feel they are the same (when it's good cinema). I also am a fan of programming, but we all are, aren't we?</div>
     
     <h1>The Index</h1>
-    <div class="container-fluid">
+    
+    <table class="table table-striped table-dark">
+        <thead>
+            <tr>
+                <th scope="col">Week</th>
+                <th scope="col">Team</th>
+                <th scope="col">Assignment</th>
+                <th scope="col">Project</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">02</th>
+                <td><a href="week02/week02.php">week02.php prep</a></td>
+                <td><a href="week02/home.php">week02 team assignment</a></td>
+                <td>(This page is the project)</td>    
+            </tr>
+            <tr>   
+                <th scope="row">03</th>
+                <td><a href="comingsoon.html">Team Activity</a></td>
+                <td><a href="comingsoon.html">Assignment</a></td>
+                <td><a href="comingsoon.html">Project</a></td>
+            </tr>
+            <tr>   
+                <th scope="row">04</th>
+                <td><a href="comingsoon.html">Team Activity</a></td>
+                <td><a href="comingsoon.html">Assignment</a></td>
+                <td><a href="comingsoon.html">Project</a></td>
+        </tr>
+            <tr>   
+            <th scope="row">05</th>
+            <td><a href="comingsoon.html">Team Activity</a></td>
+            <td><a href="comingsoon.html">Assignment</a></td>
+            <td><a href="comingsoon.html">Project</a></td>
+        </tr>
+            <tr>   
+            <th scope="row">06</th>
+            <td><a href="comingsoon.html">Team Activity</a></td>
+            <td><a href="comingsoon.html">Assignment</a></td>
+            <td><a href="comingsoon.html">Project</a></td>
+        </tr>
+            <tr>   
+            <th scope="row">07</th>
+            <td><a href="comingsoon.html">Team Activity</a></td>
+            <td><a href="comingsoon.html">Assignment</a></td>
+            <td><a href="comingsoon.html">Project</a></td>
+        </tr>
+            <tr>   
+            <th scope="row">08</th>
+            <td><a href="comingsoon.html">Team Activity</a></td>
+            <td><a href="comingsoon.html">Assignment</a></td>
+            <td><a href="comingsoon.html">Project</a></td>
+        </tr>
+            <tr>   
+            <th scope="row">09</th>
+            <td><a href="comingsoon.html">Team Activity</a></td>
+            <td><a href="comingsoon.html">Assignment</a></td>
+            <td><a href="comingsoon.html">Project</a></td>
+        </tr>
+            <tr>   
         
-            <h2>Week 02</h2>
-            <p><a href="week02/week02.php">week02.php prep</a></p>
-            <p><a href="week02/home.php">week02 team assignment</a></p>
+            <th scope="row">10</th>
+            <td><a href="comingsoon.html">Team Activity</a></td>
+            <td><a href="comingsoon.html">Assignment</a></td>
+            <td><a href="comingsoon.html">Project</a></td>
+        </tr>
+            <tr>   
         
-            <h2>Week 03</h2>
-            <p><a href="comingsoon.html">Team Activity</a></p>
-            <p><a href="comingsoon.html">Assignment</a></p>
-            <p><a href="comingsoon.html">Project</a></p>
+            <th scope="row">11</th>
+            <td><a href="comingsoon.html">Team Activity</a></td>
+            <td><a href="comingsoon.html">Assignment</a></td>
+            <td><a href="comingsoon.html">Project</a></td>
+        </tr>
+            <tr>   
         
-            <h2>Week 04</h2>
-            <p><a href="comingsoon.html">Team Activity</a></p>
-            <p><a href="comingsoon.html">Assignment</a></p>
-            <p><a href="comingsoon.html">Project</a></p>
+            <th scope="row">12</th>
+            <td><a href="comingsoon.html">Team Activity</a></td>
+            <td><a href="comingsoon.html">Assignment</a></td>
+            <td><a href="comingsoon.html">Project</a></td>
+        </tr>
+            <tr>   
         
-            <h2>Week 05</h2>
-            <p><a href="comingsoon.html">Team Activity</a></p>
-            <p><a href="comingsoon.html">Assignment</a></p>
-            <p><a href="comingsoon.html">Project</a></p>
+                <th scope="row">13</th>
+                <td><a href="comingsoon.html">Team Activity</a></td>
+                <td><a href="comingsoon.html">Assignment</a></td>
+                <td><a href="comingsoon.html">Project</a></td>
+        </tr>
+            <tr>   
         
-            <h2>Week 06</h2>
-            <p><a href="comingsoon.html">Team Activity</a></p>
-            <p><a href="comingsoon.html">Assignment</a></p>
-            <p><a href="comingsoon.html">Project</a></p>
-        
-            <h2>Week 07</h2>
-            <p><a href="comingsoon.html">Team Activity</a></p>
-            <p><a href="comingsoon.html">Assignment</a></p>
-            <p><a href="comingsoon.html">Project</a></p>
-        
-            <h2>Week 08</h2>
-            <p><a href="comingsoon.html">Team Activity</a></p>
-            <p><a href="comingsoon.html">Assignment</a></p>
-            <p><a href="comingsoon.html">Project</a></p>
-        
-        
-            <h2>Week 09</h2>
-            <p><a href="comingsoon.html">Team Activity</a></p>
-            <p><a href="comingsoon.html">Assignment</a></p>
-            <p><a href="comingsoon.html">Project</a></p>
-        
-        
-            <h2>Week 10</h2>
-            <p><a href="comingsoon.html">Team Activity</a></p>
-            <p><a href="comingsoon.html">Assignment</a></p>
-            <p><a href="comingsoon.html">Project</a></p>
-        
-        
-            <h2>Week 11</h2>
-            <p><a href="comingsoon.html">Team Activity</a></p>
-            <p><a href="comingsoon.html">Assignment</a></p>
-            <p><a href="comingsoon.html">Project</a></p>
-        
-        
-            <h2>Week 12</h2>
-            <p><a href="comingsoon.html">Team Activity</a></p>
-            <p><a href="comingsoon.html">Assignment</a></p>
-            <p><a href="comingsoon.html">Project</a></p>
-        
-        
-            <h2>Week 13</h2>
-            <p><a href="comingsoon.html">Team Activity</a></p>
-            <p><a href="comingsoon.html">Assignment</a></p>
-            <p><a href="comingsoon.html">Project</a></p>
-        
-        
-            <h2>Week 14</h2>
-            <p><a href="comingsoon.html">Team Activity</a></p>
-            <p><a href="comingsoon.html">Assignment</a></p>
-            <p><a href="comingsoon.html">Project</a></p>
-        
-    </div>
+                <th scope="row">14</th>
+                <td><a href="comingsoon.html">Team Activity</a></td>
+                <td><a href="comingsoon.html">Assignment</a></td>
+                <td><a href="comingsoon.html">Project</a></td>
+            </tr>
+        </tbody>
+    </table>
     
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
