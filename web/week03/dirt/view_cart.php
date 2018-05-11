23f2f246fc110d1a5440684fc806dd8a2dfcfd88
Matt Wyndham <matthewwyndham@gmail.com>
2018-05-10 23:15:39 -0600

    mattwyndham autocommit

--- web/week03/dirt/view_cart.php
+++ web/week03/dirt/view_cart.php
@@ -1,6 +1,8 @@
 <?php session_start(); 
     if(isset($_SESSION["cart"])) {
-       
+       foreach ($_SESSION["cart"] as $block) {
+           echo '<p>'.$block.'</p';
+       }
     } else {
         $_SESSION["cart"] = [];
     }
