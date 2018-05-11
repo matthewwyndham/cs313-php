53856b80808a2b6e2dd3980428ba3df38fa8166f
Matt Wyndham <matthewwyndham@gmail.com>
2018-05-10 23:14:25 -0600

    mattwyndham autocommit

--- web/week03/dirt/add_to_cart.php
+++ web/week03/dirt/add_to_cart.php
@@ -1,7 +1,7 @@
 <?php session_start(); 
     if(isset($_SESSION["cart"])) {
         array_push($_SESSION["cart"], $_GET["block"]);
-        header('https://uroboros-grey.herokuapp.com/week03/store.php');
+        header("Location: https://uroboros-grey.herokuapp.com/week03/store.php");
         die();
     } else {
         $_SESSION["cart"] = [];
