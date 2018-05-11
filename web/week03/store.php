e97d8ff1f932932b71b0296be79a9342b6701082
Matt Wyndham <matthewwyndham@gmail.com>
2018-05-10 21:48:24 -0600

    mattwyndham autocommit

--- web/week03/store.php
+++ web/week03/store.php
@@ -12,7 +12,7 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 </head>
     <body>
-        <?php require header.php ?>
+        <?php require 'dirt/header.php' ?>
         <main>
             <input type="hidden" name="item" value="" />
             <h1>Dirt<button onclick="" button></button></h1>
