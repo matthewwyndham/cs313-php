02ef757ee259960c0bd2e22cbfb3e1718911a9e2
Matt Wyndham <matthewwyndham@gmail.com>
2018-05-10 23:29:42 -0600

    mattwyndham autocommit

--- web/week03/dirt/remove.php
+++ web/week03/dirt/remove.php
@@ -1,6 +1,6 @@
 <?php session_start(); 
     if(isset($_SESSION["cart"])) {
-        unset($_SESSION["cart"][$_GET["index"]]);
+        unset($_SESSION["cart"][(int)$_GET["index"]]);
         array_values($_SESSION["cart"]);
         header("Location: https://uroboros-grey.herokuapp.com/week03/dirt/view_cart.php");
         die();
