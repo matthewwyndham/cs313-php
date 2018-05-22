c68072c6103477633d9cb360c163cbb5f6984c66
Matt Wyndham <matthewwyndham@gmail.com>
2018-05-22 15:50:02 -0600

    mattwyndham autocommit

--- web/index.php
+++ web/index.php
@@ -52,7 +52,7 @@
             </tr>
             <tr>   
                 <th scope="row">05</th>
-                <td><a href="week05/prepare.php">Prepare</a> - <a href="comingsoon.html">Team Activity</a></td>
+                <td><a href="week05/prepare.php">Prepare</a> - <a href="week05/team.php">Team Activity</a></td>
                 <td><a href="comingsoon.html">Assignment</a></td>
                 <td><a href="comingsoon.html">Project</a></td>
             </tr>
