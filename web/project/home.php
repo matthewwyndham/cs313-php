8c002b0834b855ce2fd10acb8227930b17972fe0
Matt Wyndham <matthewwyndham@gmail.com>
2018-05-31 15:50:07 -0600

    Thu 05/31/2018::15:50:07.51

--- web/project/home.php
+++ web/project/home.php
@@ -1,4 +1,21 @@
-<?php require('/project/res/php/database.php');
+<?php session_start(); ?>
+<?php
+    # this only works for Heroku
+    # but it's really nice
+    $dbUrl = getenv('DATABASE_URL');
+
+    $dbopts = parse_url($dbUrl);
+
+    $dbHost = $dbopts["host"];
+    $dbPort = $dbopts["port"];
+    $dbUser = $dbopts["user"];
+    $dbPassword = $dbopts["pass"];
+    $dbName = ltrim($dbopts["path"],'/');
+
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
 <?php
     if(isset($_GET['team_choice'])) {
         $search = htmlspecialchars($_GET['team_choice']);        
