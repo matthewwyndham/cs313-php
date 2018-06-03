32fd8aa8d82dbddc4b4afd8d3256ac7256d6d4dc
Matt Wyndham <matthewwyndham@gmail.com>
2018-06-03 13:03:11 -0600

    Auto::Sun 06/03/2018::13:03:11.24

--- web/project/home.php
+++ web/project/home.php
@@ -106,7 +106,7 @@
                             echo $row['content'];    
                             echo '</div>'; # end card-text
                             
-                            echo '<a href="#" class="btn btn-primary float-right">Reply</a>'; 
+                            #echo '<a href="#" class="btn btn-primary float-right">Reply</a>'; 
                             
                             echo '</div>'; # end card-body
 
